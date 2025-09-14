01efe2773598bb99549b9973bcdab31203dba6c0
yasser116926 <yasserbasher222@gmail.com>
2025-09-15 01:12:07 +0300

    Update simple_interest.py

--- simple_interest.py
+++ simple_interest.py
@@ -1,8 +1,9 @@
-principal = 1000      
-rate = 0.05           
-time = 3              
+principal = 1000
+rate = 0.05
+time = 3
 
 interest = principal * rate * time
 
-print(f"The simple interest is: {interest}")
+print(f"principal = {principal}, rate = {rate}, and time = {time} )
+
 
